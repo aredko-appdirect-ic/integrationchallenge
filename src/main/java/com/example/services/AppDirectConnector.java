12a1edaa7abe689d6a22d8b3a90180c58a963c2b
Andriy Redko <dr_reta@yahoo.com>
2014-10-18 21:23:09 -0400

    Adding response reader

--- src/main/java/com/example/services/AppDirectConnector.java
+++ src/main/java/com/example/services/AppDirectConnector.java
@@ -1,13 +1,14 @@
 package com.example.services;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.Logger;
 
 import javax.inject.Named;
-
-import com.example.rs.SubscriptionRestService;
 
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.basic.DefaultOAuthConsumer;
@@ -21,22 +22,32 @@ public class AppDirectConnector {
 	
 	private final String oauthConsumerKey = "integrationchallenge-15821";
 	private final String oauthConsumerSecret = "LUtZSr5RrIQ2Ln84";
-	private final String endpoint = "https://www.appdirect.com/rest/api/";	
 	
 	public Object get( final String eventUrl ) throws IOException, OAuthException {
 		final OAuthConsumer consumer = new DefaultOAuthConsumer( oauthConsumerKey, oauthConsumerSecret );
 		
 		final URL url = new URL( eventUrl );
-		LOG.info( "Requeting URL: " + eventUrl );
+		LOG.info( "Requesting URL: " + eventUrl );
 		
 		final HttpURLConnection request = ( HttpURLConnection )url.openConnection();
 		consumer.sign( request );
 		request.connect();
 		
 		try {
-			Object response = request.getContent();
-			LOG.info( "Response received:" + response );
-			return response;
+			final Object response = request.getContent();			
+			final StringBuilder sb = new StringBuilder();
+			
+			try( BufferedReader reader = new BufferedReader( new InputStreamReader( ( InputStream )response ) ) ) {
+				String line = reader.readLine();
+				
+				while( line != null ) {
+					sb.append( line );
+					line = reader.readLine();
+				}
+			}
+			
+			LOG.info( "Response received:" + sb.toString() );
+			return sb.toString();
 		} finally {
 			request.disconnect();
 		}
